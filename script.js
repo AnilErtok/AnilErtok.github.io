b5782ba1668a0522116d7c933cef8254ad1ec375
AnilErtok <huseyin@sakadc.com.tr>
2025-11-26 14:45:33 +0300

    test223

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-// Smooth scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+// Smooth Scroll
+document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', e => {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
+
+// Aktif menü
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll("section");
+    let scrollPos = window.scrollY + 120;
+
+    sections.forEach(sec => {
+        if (scrollPos > sec.offsetTop && scrollPos < sec.offsetTop + sec.offsetHeight) {
+            document.querySelectorAll('.nav-link').forEach(a => a.classList.remove('active'));
+            document.querySelector('.nav-link[href="#' + sec.id + '"]').classList.add('active');
+        }
     });
 });
